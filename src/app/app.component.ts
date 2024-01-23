f9aa256be1b71906cff37198fadf492e440afbf9
Titusrono <tituskrono04@gmail.com>
2024-01-23 09:31:35 +0300

    developer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ServicesComponent } from './services/services.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,HomeComponent,AboutComponent,ContactComponent,ProjectsComponent,ServicesComponent, RouterOutlet],
+  imports: [CommonModule, NavBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': '', component:HomeComponent},
+    {'path': 'about', component:AboutComponent},
+    {'path': 'contact', component:ContactComponent},
+    {'path': 'projects', component:ProjectsComponent},
+    {'path': 'services', component:ServicesComponent}
+];
